dcb4033bfc7bab284d1986f02934397eb378d312
Nikhil-228 <nikhilachu.228@gmail.com>
2024-12-11 13:59:13 +0530

    first commit

---
+++ app_8/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from app_8 .models import logintable,usertable,personaltable
+
+
+# Register your models here.
+admin.site.register(logintable)
+admin.site.register(usertable)
+admin.site.register(personaltable)
